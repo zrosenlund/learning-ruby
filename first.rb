1dc6a6515d1dc348d28aa4f7dec0cf08e37a6ea6
[zrosenlund] <[zrosenlund@mail.greenriver.edu]>
2018-10-01 14:29:28 -0700

    created basic ruby methods

--- first.rb
+++ first.rb
@@ -1,3 +1,22 @@
-name = "Zachary"
+def sum(array)
+ sum = 0
+ array.each do |item|
+   sum = sum + item
+ end
 
-puts "Hello, #{name}. It's a nice day today"  
+ return sum
+end
+
+def fancy_sum(array)
+  array.inject(0, :+)
+end     
+
+puts sum([1, 2, 3, 4, 5])
+puts fancy_sum([1, 2, 3, 4, 5])  
+
+def max_2_sum(array)
+  sorted = array.sort
+  sorted[-1] + sorted[-2]
+end
+
+puts max_2_sum([1, 5, 4, 2, 3])
